94658ce394419f2bbe337c3305fcf99e4e8cb55e
Thibault HERVIER <thibault.hervier@ens-lyon.fr>
2023-07-03 13:33:23 +0200

    script to compute counterfactuals

---
+++ counterfactuals.py
@@ -0,0 +1,19 @@
+from src import *
+
+D01 = np.load("./save/D01.npy")
+D02 = np.load("./save/D12.npy")
+D12 = np.load("./save/D12.npy")
+
+Counter01 = np.argmin(D01, axis=1)
+np.save("./save/Counter01.npy", Counter01)
+Counter02 = np.argmin(D02, axis=1)
+np.save("./save/Counter02.npy", Counter02)
+Counter12 = np.argmin(D12, axis=1)
+np.save("./save/Counter12.npy", Counter12)
+Counter10 = np.argmin(D01, axis=0)
+np.save("./save/Counter10.npy", Counter10)
+Counter20 = np.argmin(D02, axis=0)
+np.save("./save/Counter20.npy", Counter20)
+Counter21 = np.argmin(D12, axis=0)
+np.save("./save/Counter21.npy", Counter21)
+
